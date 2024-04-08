b180520595eaa6407fc11ae113e29438dafd2016
Patryck Silva <patrycksilva70@gmail.com>
2024-04-08 00:03:26 -0300

    fix: fixing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 
 import "@/styles/globals.scss";
 import Providers from "@/providers";
 import { ReactElement } from "react";
-import { cn } from "@/lib/utils";
 import EllipseBackground from "@/components/Backgrounds/EllipseBackground";
 
 export const metadata: Metadata = {
@@ -21,11 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(
-          "relative min-h-[200vh] bg-background font-sans antialiased",
-          GeistSans.className,
-          GeistMono.className,
-        )}
+        className={`relative min-h-[200vh] bg-background font-sans antialiased`}
       >
         <aside className={`fixed -right-10 -top-10 opacity-90`}>
           <EllipseBackground />
--- src/template/index.tsx
+++ src/template/index.tsx
@@ -1,9 +1,13 @@
 import { ReactElement } from "react";
 import Header from "./partials/Header";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
 const MainTemplate = ({ children }: { children: ReactElement }) => {
   return (
-    <main className={`container flex flex-col`}>
+    <main
+      className={`container flex flex-col ${GeistMono.className} ${GeistSans.className}`}
+    >
       <Header />
       {children}
     </main>
